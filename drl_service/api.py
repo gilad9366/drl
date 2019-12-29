e6d7d10bd5927caae5e3b6539a6401e6c764bafd
Gilad Sever <gilad.sever@stratoscale.com>
2019-12-29 09:58:45 +0200

    Retry on connection error from redis

--- drl_service/api.py
+++ drl_service/api.py
@@ -9,23 +9,27 @@ app = Flask(__name__)
 jsonrpc = JSONRPC(app, '/api')
 redis = Redis(host='redis', port=6379)
 
+RATE_LIMIT = 500
+RATE_TIME = 60
+
 
 @jsonrpc.method('App.handle_request')
 def handle_request(user_id):
-    ok = check_rate()
+    ok = check_rate(user_id)
     if not ok:
         return False
     pipe = redis.pipeline()
     pipe.incr(user_id)
-    pipe.expire(user_id, 500)
+    pipe.expire(user_id, RATE_TIME)
     pipe.execute()
     return True
 
 
 @retry(stop_max_attempt_number=3)
 def check_rate(user_id):
-    rate = redis.get(user_id)
-    if rate >= 499:
+    # The rate may still be unset
+    rate = redis.get(user_id) or 0
+    if int(rate) >= RATE_LIMIT:
         return False
     else:
         return True
